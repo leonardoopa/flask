d8ca69fc2ca6a1dbe794fd0090da33e0b4663e42
Leonardo Melo <leonardo.melo@gdstecnologia.com.br>
2025-07-22 20:29:41 -0300

    feat: create jogo Class

--- jogoteca.py
+++ jogoteca.py
@@ -1,11 +1,26 @@
 from flask import Flask, render_template
 
+class Jogo:
+    def __init__(self,nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+
+
 
 app = Flask(__name__)
 
 @app.route('/inicio')
 def ola_mundo():
-    return render_template('lista.html')
+
+    jogo1 = Jogo('God of War', 'Ação', 'PS4')
+    jogo2 = Jogo('Skyrim', 'RPG', 'PC')
+    jogo3 = Jogo('Valorant', 'FPS', 'PC')
+
+    lista = [jogo1, jogo2, jogo3]
+
+    return render_template('lista.html', titulo='Jogoteca', jogos=lista)
 
 
 if __name__ == '__main__':
